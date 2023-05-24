b564da1b28810e9f15de2b3439f40c1f7d618c02
Estiben Fernández <efernandeze@unal.edu.co>
2023-05-23 23:08:12 -0500

    Now responsive to mobiles

--- public/JS/index.js
+++ public/JS/index.js
@@ -4,10 +4,10 @@ const btnEnviar=document.querySelector('#btnEnviar')
 const mensajes=document.querySelector('#mensajes')
 const escribiendo=document.querySelector('#escribiendo')
 
-const token="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"
+const token="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"
 
 //Mensaje de bienvenida
-mensajes.innerHTML+='<li><strong>CHAT BOT: </strong>¡Hola! Mi nombre es CHAT BOT, tu asistente virtual. Estoy aquí para ayudarte en todo lo que necesites. Si tienes alguna pregunta o necesitas alguna asistencia, no dudes en escribirme. ¡Estoy aquí para ayudarte!</li>'
+mensajes.innerHTML+='<li><strong>CHAT BOT: </strong> Hi! My name is CHAT BOT, your virtual assistant. I´m here to help you with anything you need. If you have any questions or need any assistance, don´t hesitate to write to me. I´m here to help you!</li>'
 
 btnEnviar.addEventListener('click',()=>{
     //recuperamos el mensaje del input
@@ -15,7 +15,7 @@ btnEnviar.addEventListener('click',()=>{
     
     if(mensajeUsuario!==''){
         //Mostremos en el chat el mensaje
-        mensajes.innerHTML+=`<li><strong>USUARIO: </strong> ${mensajeUsuario}</li>`
+        mensajes.innerHTML+=`<li><strong>USER: </strong> ${mensajeUsuario}</li>`
         
 
         //Mostramos que el chat box está escribiendo mientras esperamos la respuesta
@@ -26,7 +26,7 @@ btnEnviar.addEventListener('click',()=>{
             method:'POST',
             headers:{
                 'Content-Type':'application/json', // Tipo de contenido de la solicitud
-                'Authorization': "Bearer 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" // Encabezados adicionales, como el token de autorización
+                'Authorization': "Bearer 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" // Encabezados adicionales, como el token de autorización
             },
             body:JSON.stringify({
                 "model":"gpt-3.5-turbo",
